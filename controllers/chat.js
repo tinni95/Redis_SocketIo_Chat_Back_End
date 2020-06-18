df64693d9d5c9f6ace2c0b90ddf22b4fd51c1bf1
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-18 16:51:45 +0200

    introducing joining to a channe l to avoid unvolontary message spreading

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,19 +1,19 @@
-const redis = require("../redisClient")
-const io = require("../socket")
+const redis = require("../redisClient");
+const io = require("../socket");
 
 exports.getChat = (req, res, next) => {
   const clientId = req.params.clientId;
-  redis.getMessages(clientId, 0)
-    .then(chat => {
+  redis
+    .getMessages(clientId, 0)
+    .then((chat) => {
       if (!chat) {
-        const error = new Error('Could not find chat.');
+        const error = new Error("Could not find chat.");
         error.statusCode = 404;
         throw error;
       }
-      console.log(chat)
-      res.status(200).json({ message: 'chat fetched.', chat: chat });
+      res.status(200).json({ message: "chat fetched.", chat: chat });
     })
-    .catch(err => {
+    .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
@@ -22,31 +22,30 @@ exports.getChat = (req, res, next) => {
 };
 
 exports.postMessage = (req, res, next) => {
-    const isAdmin = req.isOperator;
-    const message = req.body.message;
-    const roomId = req.params.clientId;
-    const timestamp = Date.now();
-    console.log("socket", {isAdmin,message,roomId,timestamp})
-    io.getIO().emit("new message",{
-      isAdmin,
-      message,
-      roomId,
-      timestamp
-    });
-    redis.pushMessage({ timestamp,roomId,isAdmin, message})
-    .then(len=>{
+  const isAdmin = req.isOperator;
+  const message = req.body.message;
+  const roomId = req.params.clientId;
+  const timestamp = Date.now();
+  redis
+    .pushMessage({ timestamp, roomId, isAdmin, message })
+    .then((len) => {
       if (!len) {
-        const error = new Error('Could not post message.');
+        const error = new Error("Could not post message.");
         error.statusCode = 500;
         throw error;
       }
-      res.status(200).json({ message: 'message sent.' });
+      io.getIO().to(roomId).emit("new message", {
+        isAdmin,
+        message,
+        roomId,
+        timestamp,
+      });
+      res.status(200).json({ message: "message sent." });
     })
-    .catch(err => {
+    .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
     });
-  };
-  
+};
--- index.js
+++ index.js
@@ -1,54 +1,62 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    redis= require("./redisClient"),
-    config= require("./constants/config"),
-    authRoutes = require("./routes/auth"),
-    clientsRoutes = require("./routes/clients"),
-    chatRoutes = require("./routes/chat"),
-    mongoose= require('mongoose');
-    
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  redis = require("./redisClient"),
+  config = require("./constants/config"),
+  authRoutes = require("./routes/auth"),
+  clientsRoutes = require("./routes/clients"),
+  chatRoutes = require("./routes/chat"),
+  mongoose = require("mongoose");
+
 const app = express();
 
 mongoose
   .connect(
-    'mongodb+srv://tinni:Chisana95@cluster0-fqkl1.mongodb.net/test?retryWrites=true&w=majority'
-    ,{ useNewUrlParser: true, useUnifiedTopology: true }
+    "mongodb+srv://tinni:Chisana95@cluster0-fqkl1.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(result => {
+  .then((result) => {
     redis.ConnectToRedis(startApp); //initiate redis, calling startApp if connection succeeds
   })
-  .catch(err => console.log(err));
-
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json()); // application/json
-app.use((req, res, next) => { //set up header for REST API
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.use((req, res, next) => {
+  //set up header for REST API
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 //we define the routes
-app.use('/auth', authRoutes);
-app.use('/clients', clientsRoutes);
-app.use('/chat', chatRoutes);
+app.use("/auth", authRoutes);
+app.use("/clients", clientsRoutes);
+app.use("/chat", chatRoutes);
 
 function startApp(isSuccess) {
-	if (isSuccess) {
-		const server=app.listen(config.web_port, function() {
-			console.log('Server started ' + config.web_port + ' at ' +
-				(new Date().toLocaleString().substr(0, 24)));
+  if (isSuccess) {
+    const server = app.listen(config.web_port, function () {
+      console.log(
+        "Server started " +
+          config.web_port +
+          " at " +
+          new Date().toLocaleString().substr(0, 24)
+      );
     });
-    const io = require('./socket').init(server);
-    io.on('connection', (socket) => {
-      console.log("connected")
-    });
-    
-	} else {
-		console.log("Server failed to start.");
-	}
-}
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      socket.on("join room", (userId) => {
+        socket.join(userId);
+      });
 
+      socket.on("leave room", (userId) => {
+        socket.leave(userId);
+      });
+    });
+  } else {
+    console.log("Server failed to start.");
+  }
+}
